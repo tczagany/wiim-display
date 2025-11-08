22f51848a1b287c30364d2232a8a3bfed112efcc
Tamás Czagány <32109806+tczagany@users.noreply.github.com>
2025-11-08 16:59:09 +0100

    minor refactoring

--- client/src/js/clock.js
+++ client/src/js/clock.js
@@ -1,10 +1,14 @@
+// Configuration
 var clockMarginPercent = 10;
 var showSeconds = false;
 
-var hours, minutes, seconds;
-var old_hours, old_minutes, old_seconds;
+// Namespacing
+window.FlipClk = window.FlipClk || {};
 
-function initClock() {
+FlipClk.hours = 0, FlipClk.minutes = 0, FlipClk.seconds = 0;
+FlipClk.old_hours = 0, FlipClk.old_minutes = 0, FlipClk.old_seconds = 0;
+
+FlipClk.initClock = function() {
   if (showSeconds === false) {
     document.getElementById("second-digits").outerHTML = "";
   }
@@ -15,89 +19,96 @@ function initClock() {
   clock.css('transform', 'scale(' + factor + ')');
 
   var now = new Date(Date.now());
-  old_hours = hours = now.getHours();
-  old_minutes = minutes = now.getMinutes();
-  old_seconds = seconds = now.getSeconds();
+  this.old_hours = this.hours = now.getHours();
+  this.old_minutes = this.minutes = now.getMinutes();
+  this.old_seconds = this.seconds = now.getSeconds();
 
-  $('.flipclock .flip-hours .flip-clock-active .up .counter').text(('0' + (hours)).slice(-2));
-  $('.flipclock .flip-hours .flip-clock-active .down .counter').text(('0' + (hours)).slice(-2));
+  $('.flipclock .flip-hours .flip-clock-active .up .counter').text(('0' + (this.hours)).slice(-2));
+  $('.flipclock .flip-hours .flip-clock-active .down .counter').text(('0' + (this.hours)).slice(-2));
 
-  $('.flipclock .flip-minutes .flip-clock-active .up .counter').text(('0' + (minutes)).slice(-2));
-  $('.flipclock .flip-minutes .flip-clock-active .down .counter').text(('0' + (minutes)).slice(-2));
+  $('.flipclock .flip-minutes .flip-clock-active .up .counter').text(('0' + (this.minutes)).slice(-2));
+  $('.flipclock .flip-minutes .flip-clock-active .down .counter').text(('0' + (this.minutes)).slice(-2));
 
   if (showSeconds === true) {
-    $('.flipclock .flip-seconds .flip-clock-active .up .counter').text(('0' + (seconds)).slice(-2));
-    $('.flipclock .flip-seconds .flip-clock-active .down .counter').text(('0' + (seconds)).slice(-2));
+    $('.flipclock .flip-seconds .flip-clock-active .up .counter').text(('0' + (this.seconds)).slice(-2));
+    $('.flipclock .flip-seconds .flip-clock-active .down .counter').text(('0' + (this.seconds)).slice(-2));
   }
+
+  setInterval(function() {
+    FlipClk.updateClock();
+  }, 1000);
 }
 
-var x = setInterval(function() {
-  var now = new Date(Date.now());
-  hours = now.getHours();
-  minutes = now.getMinutes();
-  seconds = now.getSeconds();
+FlipClk.animateClock = function() {
+  $('.flipclock .flip-hours .flip-clock-active .up .counter').text(('0' + (this.hours)).slice(-2));
+  $('.flipclock .flip-hours .flip-clock-active .down .counter').text(('0' + (this.hours)).slice(-2));
 
-  if(old_hours != hours)
+  if(this.old_hours != this.hours)
+    $('.flipclock .flip-hours').addClass('play');
+
+  $('.flipclock .flip-minutes .flip-clock-active .up .counter').text(('0' + (this.minutes)).slice(-2));
+  $('.flipclock .flip-minutes .flip-clock-active .down .counter').text(('0' + (this.minutes)).slice(-2));
+
+  if(this.old_minutes != this.minutes)
+    $('.flipclock .flip-minutes').addClass('play');
+
+  if (showSeconds === true) {
+    $('.flipclock .flip-seconds .flip-clock-active .up .counter').text(('0' + (this.seconds)).slice(-2));
+    $('.flipclock .flip-seconds .flip-clock-active .down .counter').text(('0' + (this.seconds)).slice(-2));
+  }
+
+  if(this.old_seconds != this.seconds)
+    $('.flipclock .flip-seconds').addClass('play');
+
+  this.old_hours = this.hours;
+  this.old_minutes = this.minutes;
+  this.old_seconds = this.seconds;
+}
+
+FlipClk.updateClock = function() {
+  var now = new Date(Date.now());
+  this.hours = now.getHours();
+  this.minutes = now.getMinutes();
+  this.seconds = now.getSeconds();
+
+  if(this.old_hours != this.hours)
     $('.flipclock .flip-hours').removeClass('play');
-  
-  if(old_minutes != minutes)
+
+  if(this.old_minutes != this.minutes)
     $('.flipclock .flip-minutes').removeClass('play');
-  
-  if(old_seconds != seconds && showSeconds === true)
+
+  if(this.old_seconds != this.seconds && showSeconds === true)
     $('.flipclock .flip-seconds').removeClass('play');
 
-  if (hours == 0) {
+  if (this.hours == 0) {
     $('.flipclock .flip-hours .flip-clock-before .up .counter').text(('0' + (23)).slice(-2));
     $('.flipclock .flip-hours .flip-clock-before .down .counter').text(('0' + (23)).slice(-2));
   } else {
-    $('.flipclock .flip-hours .flip-clock-before .up .counter').text(('0' + (hours - 1)).slice(-2));
-    $('.flipclock .flip-hours .flip-clock-before .down .counter').text(('0' + (hours - 1)).slice(-2));
+    $('.flipclock .flip-hours .flip-clock-before .up .counter').text(('0' + (this.hours - 1)).slice(-2));
+    $('.flipclock .flip-hours .flip-clock-before .down .counter').text(('0' + (this.hours - 1)).slice(-2));
   }
-  
-  if (minutes == 0) {
+
+  if (this.minutes == 0) {
     $('.flipclock .flip-minutes .flip-clock-before .up .counter').text(('0' + (59)).slice(-2));
     $('.flipclock .flip-minutes .flip-clock-before .down .counter').text(('0' + (59)).slice(-2));
   } else {
-    $('.flipclock .flip-minutes .flip-clock-before .up .counter').text(('0' + (minutes - 1)).slice(-2));
-    $('.flipclock .flip-minutes .flip-clock-before .down .counter').text(('0' + (minutes - 1)).slice(-2));
+    $('.flipclock .flip-minutes .flip-clock-before .up .counter').text(('0' + (this.minutes - 1)).slice(-2));
+    $('.flipclock .flip-minutes .flip-clock-before .down .counter').text(('0' + (this.minutes - 1)).slice(-2));
   }
   
   if (showSeconds === true) {
-    if (seconds == 0) {
+    if (this.seconds == 0) {
       $('.flipclock .flip-seconds .flip-clock-before .up .counter').text(('0' + (59)).slice(-2));
       $('.flipclock .flip-seconds .flip-clock-before .down .counter').text(('0' + (59)).slice(-2));
     } else {
-      $('.flipclock .flip-seconds .flip-clock-before .up .counter').text(('0' + (seconds - 1)).slice(-2));
-      $('.flipclock .flip-seconds .flip-clock-before .down .counter').text(('0' + (seconds - 1)).slice(-2));
+      $('.flipclock .flip-seconds .flip-clock-before .up .counter').text(('0' + (this.seconds - 1)).slice(-2));
+      $('.flipclock .flip-seconds .flip-clock-before .down .counter').text(('0' + (this.seconds - 1)).slice(-2));
     }
   }
 
-  var y = setInterval(function() {
-    $('.flipclock .flip-hours .flip-clock-active .up .counter').text(('0' + (hours)).slice(-2));
-    $('.flipclock .flip-hours .flip-clock-active .down .counter').text(('0' + (hours)).slice(-2));
-    
-    if(old_hours != hours)
-      $('.flipclock .flip-hours').addClass('play');
-
-    $('.flipclock .flip-minutes .flip-clock-active .up .counter').text(('0' + (minutes)).slice(-2));
-    $('.flipclock .flip-minutes .flip-clock-active .down .counter').text(('0' + (minutes)).slice(-2));
-
-    if(old_minutes != minutes)
-      $('.flipclock .flip-minutes').addClass('play');
-
-    if (showSeconds === true) {
-      $('.flipclock .flip-seconds .flip-clock-active .up .counter').text(('0' + (seconds)).slice(-2));
-      $('.flipclock .flip-seconds .flip-clock-active .down .counter').text(('0' + (seconds)).slice(-2));
-    }
-
-    if(old_seconds != seconds)
-      $('.flipclock .flip-seconds').addClass('play');
-    
-    old_hours = hours;
-    old_minutes = minutes;
-    old_seconds = seconds;
-    clearInterval(y);
+  setTimeout(function() {
+    FlipClk.animateClock();
   }, 100);
-}, 1000);
+}
 
-initClock();
+FlipClk.initClock();
---
+++ client/src/js/index.js
@@ -0,0 +1,40 @@
+// Namespacing
+window.App = window.App || {};
+
+App.activeFrame = 'a';
+
+App.toogleIframe = function(newContent) {
+    var iframea, iframeb;
+    if (this.activeFrame == 'a') {
+        iframea = document.getElementById('content-a');
+        iframeb = document.getElementById('content-b');
+        iframea.style.opacity = 0;
+        iframeb.style.opacity = 1;
+        iframeb.src = newContent;
+        this.activeFrame = 'b';
+    }
+    else if (this.activeFrame == 'b') {
+        iframea = document.getElementById('content-a');
+        iframeb = document.getElementById('content-b');
+        iframea.style.opacity = 1;
+        iframeb.style.opacity = 0;
+        iframea.src = newContent;
+        this.activeFrame = 'a';
+    }
+}
+
+App.init = function() {
+  setTimeout(function() {
+      App.toogleIframe('/clock.html');
+  }, 2000);
+
+  setInterval(function() {
+      if (App.activeFrame == 'a') {
+          App.toogleIframe('/clock.html');
+      } else {
+          App.toogleIframe('/wiimd.html');
+      }
+  }, 5000);
+}
+
+App.init();
